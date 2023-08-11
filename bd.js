717e2edba7f188c6b7163875214e5656e29db9d8
Walisson <132668335+Murynga@users.noreply.github.com>
2023-08-11 10:51:20 +0000

    endpoint "usuario/:id" criado

--- bd.js
+++ bd.js
@@ -14,4 +14,12 @@ async function connect() {
     return res.rows;
   }
 
-  export { selectUsuarios };
+  async function selectUsuario(id) {
+    const client = await connect();
+    const query = "SELECT * FROM usuario WHERE id = $1";
+    const usuario = [id];
+    const res = await client.query(query, usuario);
+    return res.rows;
+  }
+
+  export { selectUsuarios, selectUsuario };
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express from "express";      // Requisição do pacote do express
+import express from "express";
 import dotenv from "dotenv";
-import { selectUsuarios } from "./bd.js";
-const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+import { selectUsuarios, selectUsuario } from "./bd.js";
+const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
+app.get("/", (req, res) => {
   res.json({
-    nome: "Walisson_Fernandes_Dias",      // Substitua pelo seu nome
+    nome: "Walisson_Fernandes_Dias",
   });
   console.log("Rota / solicitada");
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
+app.listen(port, () => {
   console.log(`Serviço escutando na porta:  ${port}`);
 });
 
@@ -22,6 +22,17 @@ app.get("/usuarios", async (req, res) => {
   try {
     const usuarios = await selectUsuarios();
     res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
