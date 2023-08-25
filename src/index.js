2cfbf14d37becdab41cb095abf76d71b8a2780c9
Walisson <132668335+Murynga@users.noreply.github.com>
2023-08-25 11:40:28 +0000

    conserto final

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import roteadorUsuario from "./routes/usuario.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(roteadorUsuario);
 dotenv.config();
 
 
